d579a2d1f1a3c82de049c73aa560be55a446e324
lake <102535481@qq.com>
2022-06-24 16:46:32 +0800

    样式重复引入

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import EP from 'element-plus'
-import 'element-plus/dist/index.css'
+// import 'element-plus/dist/index.css'
 import axios from 'axios';
 import MyAxios from './myaxios.d';
 import VueAxios from 'vue-axios';
 // import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
 
 const app = createApp(App)
 // for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
