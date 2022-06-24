2e8a69e6e7f06e02e1044f68e3728c2270175e52
lake <102535481@qq.com>
2022-06-24 16:35:08 +0800

    delete icons

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ import 'element-plus/dist/index.css'
 import axios from 'axios';
 import MyAxios from './myaxios.d';
 import VueAxios from 'vue-axios';
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+// import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 
 const app = createApp(App)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
-}
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//     app.component(key, component)
+// }
 app.use(router).use(VueAxios, axios)
 try {
     app.use(EP)
